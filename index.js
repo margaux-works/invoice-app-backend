7d5d409ba2da54248c8f8ad50d797e8556c67e21
Margaux <margaux.espinasse@gmail.com>
2024-11-21 11:12:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(cors()); // allows requests from all origins
+app.use(bodyParser.json());
+
+// Placeholder route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
